a89ab0c3c8301e9b9a7a5a8a62b7fe9e96bdaea1
Ricardo Sousa <A037322@ismai.pt>
2021-05-21 23:17:11 +0100

    added model country

---
+++ weather-forecaster/src/models/country.model.ts
@@ -0,0 +1,29 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Country extends Entity {
+  @property({
+    type: 'number',
+    id: true,
+    generated: false,
+    required: true,
+  })
+  id: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  country_name: string;
+
+
+  constructor(data?: Partial<Country>) {
+    super(data);
+  }
+}
+
+export interface CountryRelations {
+  // describe navigational properties here
+}
+
+export type CountryWithRelations = Country & CountryRelations;
