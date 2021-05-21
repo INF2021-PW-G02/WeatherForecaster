af285ab6827c62f6f3347451c58fbf674d73a6f2
Ricardo Sousa <A037322@ismai.pt>
2021-05-22 00:10:19 +0100

    datasource added

---
+++ weather-forecaster/src/datasources/index.ts
@@ -0,0 +1,1 @@
+export * from './weather-forecaster-db.datasource';
---
+++ weather-forecaster/src/datasources/weather-forecaster-db.datasource.ts
@@ -0,0 +1,31 @@
+import {inject, lifeCycleObserver, LifeCycleObserver} from '@loopback/core';
+import {juggler} from '@loopback/repository';
+
+const config = {
+  name: 'WeatherForecasterDB',
+  connector: 'mysql',
+  url: 'mysql://127.0.0.1:3308/weatherforecaster',
+  host: 'localhost',
+  port: 3308,
+  user: 'root',
+  password: '',
+  database: 'weatherforecaster'
+};
+
+// Observe application's life cycle to disconnect the datasource when
+// application is stopped. This allows the application to be shut down
+// gracefully. The `stop()` method is inherited from `juggler.DataSource`.
+// Learn more at https://loopback.io/doc/en/lb4/Life-cycle.html
+@lifeCycleObserver('datasource')
+export class WeatherForecasterDbDataSource extends juggler.DataSource
+  implements LifeCycleObserver {
+  static dataSourceName = 'WeatherForecasterDB';
+  static readonly defaultConfig = config;
+
+  constructor(
+    @inject('datasources.config.WeatherForecasterDB', {optional: true})
+    dsConfig: object = config,
+  ) {
+    super(dsConfig);
+  }
+}
