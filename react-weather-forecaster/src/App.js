e9bd4de1c492c84345d5ece754682a209d0166b7
Ricardo Sousa <a037322@ismai.pt>
2021-06-09 11:38:28 +0100

    added
    - edit button to weather statuses
    - icon to cities

--- react-weather-forecaster/src/App.js
+++ react-weather-forecaster/src/App.js
@@ -1,16 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
-import {Admin, Resource, ListGuesser, List} from "react-admin";
+import {Admin, Resource, ListGuesser, List, EditGuesser} from "react-admin";
 import lb4Provider from "react-admin-lb4";
+import { CountryList } from './CountriesList';
+import { CityList } from './CitiesList';
+import { WeatherInstituteList } from './WeatherInstitutesList';
+import { WeatherStatusList } from './WeatherStatusesList';
+import cityIcon from "@material-ui/icons/LocationCity";
 
 const dataProvider = lb4Provider("http://localhost:3000");
 
 const App = () => (
   <Admin dataProvider = {dataProvider}>
-    <Resource name="countries" list={ListGuesser}/>
-    <Resource name="cities" list={ListGuesser}/>
-    <Resource name="weather-institutes" list={ListGuesser}/>
-    <Resource name="weather-statuses" list={ListGuesser}/>
+    <Resource name="countries" list={CountryList}/>
+    <Resource name="cities" list={CityList} icon={cityIcon}/>
+    <Resource name="weather-institutes" list={WeatherInstituteList}/>
+    <Resource name="weather-statuses" list={WeatherStatusList} edit={EditGuesser}/>
   </Admin>
 );
 
--- react-weather-forecaster/src/WeatherStatusesList.js
+++ react-weather-forecaster/src/WeatherStatusesList.js
@@ -1,10 +1,11 @@
-import { List, Datagrid, TextField, NumberField, DateField, ReferenceField} from "react-admin";
+import { List, Datagrid, TextField, NumberField, DateField, ReferenceField, EditButton} from "react-admin";
 
 export const WeatherStatusList = props => (
     <List {...props}>
         <Datagrid rowClick="edit">
             <NumberField source="id" />
             <TextField source="weather_status" />
+            <EditButton/>
         </Datagrid>
     </List>
 );
