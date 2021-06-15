fe5c5865769b71ea2795a6164b1ec562705c03cf
Ricardo Sousa <a037322@ismai.pt>
2021-06-15 19:07:36 +0100

    preparations for the dashboard

--- react-weather-forecaster/src/dashboard.js
+++ react-weather-forecaster/src/dashboard.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Card, CardContent, CardHeader } from '@material-ui/core';
+getCities() {
 
-export default () => (
-    <Card>
-        <CardHeader title="Welcome to Weather Forecaster" />
-        <CardContent>This is where you can better visualize the 
-            contents of the DataBase and also manage the records of our API.</CardContent>
-    </Card>
-);
+}
+export default function dashboard() {
+    return(
+
+    );
+};
